6aba521594c2c43edaba67bee4e9b1b8656ea068
= <klas_hermansson@hotmail.com>
2023-10-21 17:44:53 +0200

    Added Menu for customer management
    Added a class for customer management and a menu function.

---
+++ CustomerManagement.cs
@@ -0,0 +1,45 @@
+﻿namespace WebShop6_v2;
+
+public class CustomerManagement
+{
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine($"******************************************************************");
+        Console.WriteLine($"****************************************************************** \n");
+        Console.WriteLine("Customer Account Management");
+        Console.WriteLine(" 1. Remove Customer\n");
+        Console.WriteLine(" 2. Edit username\n");
+        Console.WriteLine(" 3. Edit Password\n");
+        Console.WriteLine(" 0. Exit\n");
+        Console.WriteLine($"******************************************************************");
+        Console.WriteLine($"****************************************************************** \n");
+
+        bool validInput = int.TryParse(Console.ReadLine(), out int choice);
+        if (validInput)
+        {
+            switch (choice)
+            {
+                case 0:
+                    return;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    Menu();
+                    break;
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+            Thread.Sleep(1000);
+            Menu();
+        }
+    }
+}
